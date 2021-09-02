16882033dd8f17862b78312eced7747b1e129baa
EnergyNick <nsikachin@gmail.com>
2021-09-02 15:37:17 +0500

    Added static factories for Result

---
+++ src/SimpleResult/Settings/ResultParameters.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SimpleResult.Settings
+{
+    public class ResultParameters
+    {
+        public Func<Exception, Error> DefaultTryCatchHandler { get; set; }
+    }
+}
---
+++ src/SimpleResult/Settings/ResultSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleResult.Settings
+{
+    /// <summary>
+    /// Settings for general Result behavior in certain scenarios
+    /// </summary>
+    public static class ResultSettings
+    {
+        /// <summary>
+        /// Current parameters used in Results
+        /// </summary>
+        public static ResultParameters Parameters { get; private set; }
+
+        static ResultSettings()
+        {
+            Parameters = GetDefaultParameters();
+        }
+        
+        public static void SetupParameters(Func<ResultParameters, ResultParameters> settingsChangeFunction)
+        {
+            lock (Parameters)
+            {
+                Parameters = settingsChangeFunction(Parameters);
+            }
+        }
+
+        public static ResultParameters GetDefaultParameters() => new()
+        {
+            DefaultTryCatchHandler = ex => new ExceptionalError(ex)
+        };
+    }
+}
