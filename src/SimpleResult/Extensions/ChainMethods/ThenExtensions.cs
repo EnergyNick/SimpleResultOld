0536c25562f79a01d0727ca38913d5e509644d11
EnergyNick <nsikachin@gmail.com>
2021-09-02 15:39:56 +0500

    Base realization of functions Then and, ThenTry

---
+++ src/SimpleResult/Extensions/ChainMethods/ThenExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SimpleResult
+{
+    public static partial class ResultsExtensions
+    {
+        public static Result ThenAction(this Result input, Action continuation)
+        {
+            if (input.IsSuccess)
+                continuation();
+            return input;
+        }
+
+        public static Result<TValue> ThenAction<TValue>(this Result<TValue> input, Action<TValue> continuation)
+        {
+            if (input.IsSuccess)
+                continuation(input.ValueOrDefault);
+            return input;
+        }
+
+        public static Result<TOutput> Then<TOutput>(this Result input, Func<TOutput> continuation)
+        {
+            return input.IsSuccess
+                ? continuation().AsResult()
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+        
+        public static Result<TOutput> Then<TOutput>(this Result input, Func<Result<TOutput>> continuation)
+        {
+            return input.IsSuccess
+                ? continuation()
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+
+        public static Result<TOutput> Then<TInput, TOutput>(this Result<TInput> input,
+            Func<TInput, TOutput> continuation)
+        {
+            return input.IsSuccess
+                ? continuation(input.ValueOrDefault).AsResult()
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+        
+        public static Result<TOutput> Then<TInput, TOutput>(this Result<TInput> input,
+            Func<TInput, Result<TOutput>> continuation)
+        {
+            return input.IsSuccess
+                ? continuation(input.ValueOrDefault)
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+    }
+}
