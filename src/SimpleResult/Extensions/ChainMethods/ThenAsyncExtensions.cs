ed4076238d6e780cad03c696e769b33f9e4478b2
EnergyNick <nsikachin@gmail.com>
2021-09-02 15:40:18 +0500

    Base realization of functions ThenAsync and, ThenTryAsync

---
+++ src/SimpleResult/Extensions/ChainMethods/ThenAsyncExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleResult
+{
+    public static partial class ResultsExtensions
+    {
+        public static async Task<Result> ThenActionAsync(this Result input, Func<Task> continuation)
+        {
+            if (input.IsSuccess)
+                await continuation();
+            return input;
+        }
+
+        public static async Task<Result<TValue>> ThenActionAsync<TValue>(this Result<TValue> input, 
+            Func<TValue, Task> continuation)
+        {
+            if (input.IsSuccess)
+                await continuation(input.ValueOrDefault);
+            return input;
+        }
+
+        public static async Task<Result<TOutput>> ThenAsync<TOutput>(this Result input, 
+            Func<Task<TOutput>> continuation)
+        {
+            return input.IsSuccess
+                ? Result.Success(await continuation())
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+        
+        public static async Task<Result<TOutput>> ThenAsync<TOutput>(this Result input, 
+            Func<Task<Result<TOutput>>> continuation)
+        {
+            return input.IsSuccess
+                ? await continuation()
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+
+        public static async Task<Result<TOutput>> ThenAsync<TInput, TOutput>(this Result<TInput> input,
+            Func<TInput, Task<TOutput>> continuation)
+        {
+            return input.IsSuccess
+                ? Result.Success(await continuation(input.ValueOrDefault))
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+        
+        public static async Task<Result<TOutput>> ThenAsync<TInput, TOutput>(this Result<TInput> input,
+            Func<TInput, Task<Result<TOutput>>> continuation)
+        {
+            return input.IsSuccess
+                ? await continuation(input.ValueOrDefault)
+                : input.Convert.ToResultWithValue<TOutput>();
+        }
+    }
+}
