d6be1549f2396e03cf37a88c646218e686e70104
EnergyNick <nsikachin@gmail.com>
2021-09-06 15:43:13 +0500

    Refactoring of ReasonExtensions, added docs for few methods

--- src/SimpleResult/Extensions/ReasonExtensions.cs
+++ src/SimpleResult/Extensions/ReasonExtensions.cs
@@ -10,17 +10,22 @@ namespace SimpleResult.Extensions
         public static bool HasReasonOfType<TReason>(this IEnumerable<IReason> reasons, Func<TReason, bool> predicate)
             where TReason : IReason
         {
-            return reasons.Any(reason => 
+            return reasons.Any(reason =>
                 reason is TReason reasonOfType && (predicate == null || predicate(reasonOfType)));
         }
         
-        public static bool HasErrorOfType<TError>(this IReadOnlyList<IError> errors, Func<TError, bool> predicate) 
+        public static bool HasErrorOfType<TError>(this IEnumerable<IError> errors, Func<TError, bool> predicate) 
             where TError : IError
         {
-            var anyErrors = HasReasonOfType(errors, predicate);
-            return anyErrors || errors.Any(error => HasErrorOfType(error.CausedErrors, predicate));
+            var enumeratedReasons = errors as ICollection<IError> ?? errors.ToArray();
+            
+            var anyErrors = HasReasonOfType(enumeratedReasons, predicate);
+            return anyErrors || enumeratedReasons.Any(error => HasErrorOfType(error.CausedErrors, predicate));
         }
         
+        /// <summary>
+        /// Add reason to list and return that list. Used to improve readability of the code
+        /// </summary>
         public static List<TReason> AddNewReason<TReason>(this List<TReason> reasons, TReason newReason)
             where TReason : IReason
         {
@@ -28,6 +33,9 @@ namespace SimpleResult.Extensions
             return reasons;
         }
 
+        /// <summary>
+        /// Add reason to list and return that list. Used to improve readability of the code
+        /// </summary>
         public static List<TReason> AddNewReasons<TReason>(this List<TReason> reasons, IEnumerable<TReason> newReasons)
             where TReason : IReason
         {
