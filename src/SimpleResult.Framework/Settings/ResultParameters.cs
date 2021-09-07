52fe96d53a6e4b77c20baba6c6c36b8c1a5423b0
EnergyNick <nsikachin@gmail.com>
2021-09-07 09:56:15 +0500

    Removed Framework project

--- src/SimpleResult.Framework/Settings/ResultParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SimpleResult.Settings
-{
-    public partial class ResultParameters
-    {
-        public Func<Exception, Error> DefaultTryCatchHandler { get; set; }
-    }
-}
