e9484dcf72ff558966dd8ddc89b107e770fbf87e
EnergyNick <nsikachin@gmail.com>
2021-09-28 11:00:09 +0500

    Refactoring and move Success methods to SimpleResult.Core.
    Cleanup using statements

---
+++ src/SimpleResult.Core/Extensions/SuccessAndFailExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleResult.Core;
+
+namespace SimpleResult.Extensions
+{
+    public static class ResultsSuccessExtensions
+    {
+        /// <summary>
+        /// Call action only if <see cref="result"/> is success
+        /// </summary>
+        public static TResult OnSuccess<TResult>(this TResult result, Action<TResult> onSuccessAction)
+            where TResult : IResult
+        {
+            if (!result.IsSuccess)
+                return result;
+
+            onSuccessAction(result);
+            return result;
+        }
+
+        /// <summary>
+        /// Call action only if <see cref="result"/> is failed
+        /// </summary>
+        public static TResult OnFail<TResult>(this TResult result, Action<TResult> onFailAction)
+            where TResult : IResult
+        {
+            if (result.IsSuccess)
+                return result;
+            
+            onFailAction(result);
+            return result;
+        }
+        
+        /// <summary>
+        /// Call action only if <see cref="result"/> is failed
+        /// </summary>
+        public static TResult OnFail<TResult>(this TResult result, Action<TResult, IEnumerable<IError>> onFailAction)
+            where TResult : IResult
+        {
+            if (result.IsSuccess)
+                return result;
+            
+            onFailAction(result, result.Errors);
+            return result;
+        }
+    }
+}
--- src/SimpleResult/Reasons/Success.cs
+++ src/SimpleResult/Reasons/Success.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Collections.ObjectModel;
-using System.Linq;
 using SimpleResult.Core;
 
 namespace SimpleResult
