0caceb1b8b02dd469b025fb69b538db3a5b0a60c
EnergyNick <nsikachin@gmail.com>
2022-06-26 20:19:51 +0300

    Renaming of Manipulation methods, add inline docs

---
+++ src/SimpleResult.Core/Core/Manipulations/IConclusionManipulationMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleResult.Core.Manipulations
+{
+    /// <summary>
+    /// Providing methods of fluent api for <see cref="IResult"/>
+    /// </summary>
+    public interface IConclusionManipulationMethods<out TConclusion>
+        where TConclusion : IConclusion
+    {
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IReason"/> and updated status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new reason</returns>
+        TConclusion WithReason(IReason reason);
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IReason"/>-s and updated status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new reasons</returns>
+        TConclusion WithReasons(IEnumerable<IReason> reasons);
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IReason"/>-s and updated status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new reasons</returns>
+        TConclusion WithReasons(params IReason[] reasons);
+
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IError"/> and failed status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new error</returns>
+        TConclusion WithError(IError error);
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IError"/>-s and failed status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new errors</returns>
+        TConclusion WithErrors(IEnumerable<IError> errors);
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with appended <see cref="IError"/>-s and failed status
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new errors</returns>
+        TConclusion WithErrors(params IError[] errors);
+    }
+}
---
+++ src/SimpleResult.Core/Core/Manipulations/IConclusionManipulationMethods_TValue.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleResult.Core.Manipulations
+{
+    /// <summary>
+    /// Providing methods of fluent api for <see cref="IResult{TValue}"/>
+    /// </summary>
+    public interface IConclusionManipulationMethods<out TConclusion, in TValue>
+        : IConclusionManipulationMethods<TConclusion> where TConclusion : IResult<TValue>
+    {
+        /// <summary>
+        /// Return copy of <see cref="TConclusion"/> with changed <see cref="value"/>
+        /// </summary>
+        /// <returns>Copy of current <see cref="TConclusion"/> with new value</returns>
+        TConclusion WithValue(TValue value);
+    }
+}
