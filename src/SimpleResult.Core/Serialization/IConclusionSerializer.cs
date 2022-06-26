48b5971157a4ee899c72590c77085fa2c83f66c9
EnergyNick <nsikachin@gmail.com>
2022-06-26 19:49:35 +0300

    Rework serializer and move realization from Core

---
+++ src/SimpleResult.Core/Serialization/IConclusionSerializer.cs
@@ -0,0 +1,15 @@
+﻿using SimpleResult.Core;
+
+namespace SimpleResult.Serialization
+{
+    /// <summary>
+    /// Provide serialization methods for <see cref="IConclusion"/> types.
+    /// </summary>
+    public interface IConclusionSerializer
+    {
+        /// <summary>
+        /// Provide serialization of <see cref="IConclusion"/> types to human readable string
+        /// </summary>
+        string ConvertToHumanReadableString<TConclusion>(TConclusion conclusion) where TConclusion : IConclusion;
+    }
+}
